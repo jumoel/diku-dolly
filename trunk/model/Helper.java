21f835e9d4dd9e6cfc3b392d06b661f83dba50f7
Steen Nordsmark Pedersen <steen.nordsmark@gmail.com>
2007-05-23 10:41:45 +0000

    wrote helper, deleted Emils crap in helper (missing comments)

--- trunk/model/Helper.java
+++ trunk/model/Helper.java
@@ -8,29 +8,7 @@ import java.util.Random;
 /**
  * @author Steen
  *
- */
-	
-	/**
-	 * Gets a helping text, and points out a field on the Board,
-	 * which only has one solution.
-	 * @return a text
-	 */
-	public void getHelp() {
-	// Kald til funktion fra Solver som lokaliserer et løsbart felt.
-	// Gem dette fieldNum i en variabel.
-	int SolvedField = 0;
-	// Send denne værdi videre til view.Board, så fieldet med værdien
-	// fieldNum bliver farvet grønt.
-	
-	// String help1 = "";
-	// String help2 = "";
-	// String help3 = "";
-	// String help4 = "";
-	// String help5 = "";
-	// String help6 = "";
-	
-	}
-	
+ */	
 	
 public abstract class Helper {
 
