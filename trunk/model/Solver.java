fe5a290dbb831622281f2509ad221c8938b59503
Klaes Rasmussen <k@klaes.dk>
2007-05-23 17:06:41 +0000

    hopefully completed solverLevelTwo... without comments!!

--- trunk/model/Solver.java
+++ trunk/model/Solver.java
@@ -1,6 +1,5 @@
 /**
- * NOTE: seperate the solvers into different classes and use the abstraction level
- * to make more reuse of code.
+ * 
  */
 package model;
 
@@ -11,12 +10,12 @@ package model;
 public abstract class Solver {
        
 	/**
-	 * In calling the solver, first solverLevelOne is used, then if this is unable
-	 * to produce a unique result, call solverLevelTwo. 
+	 * In calling the solver, first solverLevelOne is used, then if this is 
+	 * unable to produce a unique result, call solverLevelTwo. 
 	 * 
-	 * @param fieldNum
-	 * @param board
-	 * @return
+	 * @param fieldNum, the integer value the specific field has on the board
+	 * @param board, the sudoku board used
+	 * @return the result of trying to solve the field, 0 if not solveable.
 	 */
 	public static int solveField(int fieldNum, Board board) {
         /**
@@ -25,18 +24,44 @@ public abstract class Solver {
          * 
          * allValues specifies all the possible values for the given field
          * possibleValues is used to store the values which are still, during
-         * the solve operation, possible for the given fiels. And then in the end
-         * this can be called upon to check for a final set of values. Then, 
-         * if this set consists of only one value, that value must be the answer.
+         * the solve operation, possible for the given fiels. And then in the 
+         * end this can be called upon to check for a final set of values. 
+         * Then, if this set consists of only one value, that value must be the
+         * answer.
          */
         int[] allValues = {1,2,3,4,5,6,7,8,9};
         int[] possibleValues = allValues;
         
 		int[] levelOneResult = solverLevelOne(fieldNum, possibleValues, board);
-		if (levelOneResult[0] != 0 && levelOneResult[1] == 0) {
-			return levelOneResult[0];
+		
+        /**
+         * 	'result' is a unique solution of the field
+         *  'count' is the number of final possible solutions for the 
+         *  field.
+         */
+		int result = 0;
+        int count = 0;
+        
+        /**
+         * Save the last possible value for the field in 'result' while
+         * counting the number of possible values.
+         */
+        for (int i = 0; i < levelOneResult.length; i++) {
+                if (levelOneResult[i] > 0) {
+                        result = possibleValues[i];
+                        count++;
+                }
+        }
+		
+        /**
+         * If there is only one possible solution using solverLevelOne,
+         * return this, otherwise try to solve using solverLevelTwo and return
+         * output form that.
+         */
+		if (count == 1) {
+			return result;
 		} else {
-			return solverLevelTwo(fieldNum, board, possibleValues);
+			return solverLevelTwo(fieldNum, board, levelOneResult);
 		}
 	}
 	
@@ -52,26 +77,19 @@ public abstract class Solver {
 		 * @return 0 if there are several possibilities,
 		 * 		  and the solution if it is singular.
 		 */
-		private static int[] solverLevelOne(int fieldNum, int[] possibleValues, Board board) {
+		private static int[] solverLevelOne(int fieldNum, int[] possibleValues,
+											Board board) {
     	   /**
-    	    * Functions to keep the row, column and quadrant of the specific field
-    	    * in local memory.
+    	    * Functions to keep the row, column and quadrant of the specific 
+    	    * field in local memory.
     	    */
                int[] row = SudokuMath.getRowFromPos(fieldNum, board);
                int[] column = SudokuMath.getColumnFromPos(fieldNum, board);
                int[] quadrant = SudokuMath.getQuadrantFromPos(fieldNum, board);
-               
-               /**
-                *  'count' is the number of final possible solutions for the field.
-                *  'result' is the the solution to the field, this is only 0 if the field
-                *  is already filled in.
-                */
-               int count = 0;
-               int[] result = new int[possibleValues.length];
 
                /**
-                * Here we filter out the values which cannot be the result, subtract
-                * them from the possible values.
+                * Here we filter out the values which cannot be the result, 
+                * subtract them from the possible values.
                 * 
                 * Cycle through the row, column and quadrant of the field.
                 * Then for all of those fields, with numbers greater than 0,
@@ -90,18 +108,11 @@ public abstract class Solver {
                            possibleValues[j] = 0;
                        }
                }
-               
                /**
-                * Save the last possible value for the field in 'result' while
-                * counting the number of possible values.
+                * Return the set of possibleValues so they can be reused in
+                * the next levels of solvers.
                 */
-               for (int i = 0; i < possibleValues.length; i++) {
-                       if (possibleValues[i] > 0) {
-                               result[count] = possibleValues[i];
-                               count++;
-                       }
-               }
-               return result;
+               return possibleValues;
        }
 
        /**
@@ -117,9 +128,48 @@ public abstract class Solver {
         * @param board
         * @return
         */
-       private static int solverLevelTwo(int fieldNum, Board board, int[] previousPossibilities) {
-    	   for (int i = 0; i < previousPossibilities.length; i++) {
-    		   
+       private static int solverLevelTwo(int fieldNum, Board board, 
+    		   							 int[] prevSolutions) {
+    	   int fieldRowNum = SudokuMath.getRowNumber(fieldNum, 
+				   board.getSettings());
+    	   int fieldColumnNum = SudokuMath.getColumnNumber(fieldNum,
+    			   board.getSettings());
+    	   int fieldQuadrantNum = SudokuMath.getQuadrantNumber(fieldNum,
+    			   board.getSettings());
+    	   int quadDim = board.getSettings().getQuadrantDimension();
+    	   int boardLength = board.getSettings().getBoardDimensions();
+    	   
+    	   int quadStartPos = ((fieldQuadrantNum % quadDim) * quadDim) + 
+    	   					  (fieldRowNum / 3 * 3 * boardLength);
+    	   
+    	   int l = prevSolutions.length;
+    	   
+    	   for (int i = 0; i < l; i++) {
+    		   if (fieldColumnNum != i) {
+    			   int[] resultsRow = 
+    				   solverLevelOne(fieldNum - fieldColumnNum + i,
+    						   prevSolutions, board);  
+    			   for (int j = 0; resultsRow[j] != 0; j++) {
+    				   prevSolutions[resultsRow[j] - 1] = 0;
+    			   }
+    		   }
+    		   if (fieldRowNum != i) {
+    			   int[] resultsColumn =
+    				   solverLevelOne(fieldColumnNum + (fieldRowNum * i), 
+    						   prevSolutions, board);
+    			   for (int j = 0; resultsColumn[j] != 0; j++) {
+    				   prevSolutions[resultsColumn[j] - 1] = 0;
+    			   }
+    		   }
+    		   if (fieldColumnNum != i && fieldRowNum != i) {
+    			   int[] resultsQuadrant = 
+    				   solverLevelOne(quadStartPos + 
+    						   (i % quadDim) * (fieldRowNum % quadDim), 
+    					   	   prevSolutions, board);
+    			   for (int j = 0; resultsQuadrant[j] != 0; j++) {
+    				   prevSolutions[resultsQuadrant[j] - 1] = 0;
+    			   }
+    		   }
     	   }
     	   return 0;
        }
