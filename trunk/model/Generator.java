eae0f57ca589d81023a45b7dac2f8ea93e0f1987
Steen Nordsmark Pedersen <steen.nordsmark@gmail.com>
2007-05-23 09:14:48 +0000

    Steen: Created generator basic funktions. need "recursion".

--- trunk/model/Generator.java
+++ trunk/model/Generator.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package model;
 
 import java.util.Random;
@@ -11,22 +8,59 @@ import java.util.Random;
  */
 public abstract class Generator {
 	
-	private Random random;
-	
-	int currentValue = 0;
-	
-	public void generate(Board board) {
-		// TODO: Implement. LAWL.
-		// TODO: random tal 0-81 som skal fjernes. få board til at fjerne, få løseren til at løse, hvis muligt, fjern tal, hvis ikke sæt det tilbage.
-		random = new Random(81);
-		
-		int a = random.nextInt();
-		
-		// Gets value a from board[] and stores it
-		currentValue = board.getValue(a); 
 
-		// removes value a from board[]
-		board.setValue(a, 0);
 	
+/**
+ * Generates a playable gameboard removing one field at a time untill the 
+ * desired difficulty is achieved.
+ * @param board
+ * @param settings
+ */
+	
+	public void generate(Board board, GameSettings settings) {
+		
+		// TODO: gør den rekursiv så den laver hele boardet.
+		
+		Random random = new Random();
+		// TODO: random(81) er et magisk tal, skal hentes fra model.gamesettings.boardSize eller lign
+		
+		/*
+		 * Selects a random place on the gameboard.
+		 */
+		int fieldId = random.nextInt(81);	
+		
+		
+		/*
+		 * If the field in empty (already removed) the following field is selected.
+		 */
+		// TODO: 80 = magic number
+		while (board.getValue(fieldId) == 0) {
+			fieldId = (fieldId + 1) % 80;
+		}
+		
+		/*
+		 * Stores the value of the currently selected field in currentValue.
+		 * Sets the fields value to 0 (empty)
+		 */
+		int currentValue = board.getValue(fieldId);
+		board.setValue(fieldId, 0);
+		
+		/*
+		 * If the selected (and removed) field cannot be solved by the sudoku 
+		 * solver, the field is reset to its previous value and the next field
+		 * in line is examined.
+		 */
+		while (Solver.solveField(fieldId, board) == 0) {
+
+			board.setValue(fieldId, currentValue);
+			fieldId = fieldId + 1;
+			while (board.getValue(fieldId) == 0) {
+				fieldId = (fieldId + 1) % 80;
+			}
+			currentValue = board.getValue(fieldId);
+			board.setValue(fieldId, 0);
+				
+			}
+
 	}
 }
