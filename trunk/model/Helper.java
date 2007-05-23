2fa27ad21fefbaae60ef0c87af2acc46b7797a8d
Emil Erik Hansen <emil@gnueless.com>
2007-05-23 09:58:30 +0000

    semi-started model/Helper.java and view/Board.java

--- trunk/model/Helper.java
+++ trunk/model/Helper.java
@@ -8,5 +8,27 @@ package model;
  *
  */
 public class Helper {
-
+	
+	/**
+	 * Gets a helping text, and points out a field on the Board,
+	 * which only has one solution.
+	 * @return a text
+	 */
+	public void getHelp() {
+	// Kald til funktion fra Solver som lokaliserer et løsbart felt.
+	// Gem dette fieldNum i en variabel.
+	int SolvedField = 0;
+	// Send denne værdi videre til view.Board, så fieldet med værdien
+	// fieldNum bliver farvet grønt.
+	
+	// String help1 = "";
+	// String help2 = "";
+	// String help3 = "";
+	// String help4 = "";
+	// String help5 = "";
+	// String help6 = "";
+	
+	}
+	
+	
 }
--- trunk/view/Board.java
+++ trunk/view/Board.java
@@ -3,10 +3,17 @@
  */
 package view;
 
+import javax.swing.JPanel;
+
 /**
  * @author Julian
  *
  */
-public class Board {
+public class Board extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7704761091317274700L;
 
 }
