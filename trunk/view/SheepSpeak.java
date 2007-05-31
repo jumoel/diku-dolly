99f87cea860be8b430234480edaa223aa14c7a1b
Emil Erik Hansen <emil@gnueless.com>
2007-05-31 16:03:53 +0000

    JULIAN OWNS THE 100!

---
+++ trunk/view/SheepSpeak.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package view;
+
+import javax.swing.JPanel;
+
+/**
+ * @author emil
+ *
+ */
+public class SheepSpeak extends JPanel {
+	private String text;
+	public SheepSpeak() {
+		text = "Hej, og velkommen til SudokuLOL!";
+	}
+	
+	
+	
+	public void setText(String text) {
+		
+	}
+}
