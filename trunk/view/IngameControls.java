0b4411cd5848dcdfc305f67d58e3311961d36bf5
Julian Møller <julian@julianmoeller.dk>
2007-06-06 08:46:17 +0000

    Commented.

--- trunk/view/IngameControls.java
+++ trunk/view/IngameControls.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package view;
 
 import java.awt.Component;
@@ -11,18 +8,20 @@ import controller.DifficultyAction;
 import controller.HelpAction;
 
 /**
- * @author Julian
- *
+ * The ingamecontrols (Help- and New Game-button)
  */
 public class IngameControls extends JPanel {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -1310963790966866671L;
 	
 	private DifficultyAction difficultyAction;
 	private HelpAction helpAction;
 
+	/**
+	 * Creates the controls.
+	 * @param frame The frame the screen should be added to.
+	 * @param difficultyAction The DifficultyAction showing the New Game-screen.
+	 * @param helpAction The HelpAction handling requests for help.
+	 */
 	public IngameControls(final Component frame,
 			DifficultyAction difficultyAction,
 			HelpAction helpAction) {
@@ -32,6 +31,9 @@ public class IngameControls extends JPanel {
 		this.difficultyAction = difficultyAction;
 		this.helpAction = helpAction;
 		
+		/*
+		 * Create the buttons and add the actions.
+		 */
 		SudokuButton helpButton = new SudokuButton("help.png");
 		helpButton.addActionListener(this.helpAction);
 		this.add(helpButton);
@@ -41,14 +43,21 @@ public class IngameControls extends JPanel {
 		this.add(newButton);
 		
 		this.setOpaque(false);
-		
 		this.setSize(300, 100);
 	}
 
+	/**
+	 * Gets the DifficultyAction associated with the controls.
+	 * @return The DifficultyAction
+	 */
 	public DifficultyAction getDifficultyAction() {
 		return difficultyAction;
 	}
 
+	/**
+	 * Gets the HelpAction associated with the controls.
+	 * @return The HelpAction
+	 */
 	public HelpAction getHelpAction() {
 		return helpAction;
 	}
